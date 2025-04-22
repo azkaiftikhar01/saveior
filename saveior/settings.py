9c4456302866f85a903d4cd60aa590b13c6e8af5
azka <iftikharazka1@gmail.com>
2025-04-22 22:16:21 +0500

    crffc

--- saveior/settings.py
+++ saveior/settings.py
@@ -144,7 +144,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Login/Logout URLs
 LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 
 # Currency API Settings
